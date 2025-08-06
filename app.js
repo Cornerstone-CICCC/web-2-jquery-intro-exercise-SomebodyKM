1de6b58ed6bb0ac53bf04b1ad05a3c2afd4a8fb3
Miya Kuo <miya3927@gmail.com>
2025-08-05 20:52:16 -0700

    Finished exercise

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-$(function() {
+$(function () {
   // YOUR CODE HERE
+  $('#toggleButton').on('click', function () {
+    $('#myParagraph').toggle()
+  })
+
+  $('#colorButton').on('click', function () {
+    $('#colorDiv').css('background-color', 'yellow')
+  })
+
+  $('#addClassButton').on('click', function () {
+    $('#classDiv').addClass('newClass')
+  })
+
+  $('#removeClassButton').on('click', function () {
+    $('#classDiv').removeClass('newClass')
+  })
+
+  $('#fadeInButton').on('click', function () {
+    $('#fadeDiv').fadeIn()
+  })
+
+  $('#fadeOutButton').on('click', function () {
+    $('#fadeDiv').fadeOut()
+  })
+
+  $('#slideUpButton').on('click', function () {
+    $('#slideDiv').slideUp()
+  })
+
+  $('#slideDownButton').on('click', function () {
+    $('#slideDiv').slideDown()
+  })
 })
